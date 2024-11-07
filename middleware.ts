3ec98d55f8cf9fc138d89a42bed145c39414c377
Agence Web La Capsule <132720142+Lacapsule66@users.noreply.github.com>
2024-11-07 13:22:25 +0100

    ok

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,9 @@
-// middleware.ts
+import NextAuth from "next-auth";
 
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+import { authConfig } from "@/app/(auth)/auth.config";
 
-export async function middleware(req: NextRequest) {
-  // Token from Auth.js (NextAuth)
-  const token = await getToken({
-    req,
-    secret: "JT+B2CM7T62wmU+eHExjDBBzw25lENSBYIZFAmjiJ2s=",
-  });
-
-  // Définir les chemins que vous souhaitez exclure de la protection
-  const excludedPaths = ["/login", "/signup", "/public-page", "/register"];
-
-  // Vérifie si le chemin fait partie des exceptions
-  if (excludedPaths.some((path) => req.nextUrl.pathname.startsWith(path))) {
-    return NextResponse.next();
-  }
-
-  // Redirige vers la page de connexion si pas de session
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  return NextResponse.next();
-}
+export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: [
-    // Cela inclut toutes les routes sauf celles spécifiées dans excludedPaths.
-    "/((?!_next/static|favicon.ico).*)",
-  ],
+  matcher: ["/", "/:id", "/api/:path*", "/login", "/register"],
 };
